048cbab6f381d629ae68c38652241ff0b69df767
theTrine333 <trinekaka@gmail.com>
2025-02-18 12:22:58 +0300

    Removed bad importts

--- app/library/_layout.tsx
+++ app/library/_layout.tsx
@@ -1,15 +1,10 @@
 import { ThemedView } from "@/components/ThemedView";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Downloads from "./downloads";
 import Playlists from "./playlists";
 import { useColorScheme } from "react-native";
-import { Colors } from "@/constants/Colors";
 import Favourites from "./favourites";
-import { ThemedText } from "@/components/ThemedText";
 import SegmentedControl from "@/components/SegmentedControls";
 import { useState } from "react";
-
-const Tab = createMaterialTopTabNavigator();
 
 export default function Tabs() {
   const theme = useColorScheme() ?? "light";
@@ -28,25 +23,5 @@ export default function Tabs() {
         <Playlists />
       )}
     </ThemedView>
-    // <Tab.Navigator
-    //   screenOptions={{
-    //     tabBarStyle: {
-    //       backgroundColor: Colors[theme ?? "light"].background,
-    //       width: "100%",
-    //     },
-    //     tabBarScrollEnabled: true,
-    //     tabBarShowIcon: true,
-    //     tabBarActiveTintColor:
-    //       theme == "light"
-    //         ? Colors.light.tabIconDefault
-    //         : Colors.Slider.primary,
-    //     tabBarInactiveTintColor: "grey",
-    //   }}
-    // >
-    //   <Tab.Screen name="Downloads" component={Downloads} />
-    //   <Tab.Screen name="Favourites" component={Favourites} />
-    //   <Tab.Screen name="Playlists" component={Playlists} />
-    // </Tab.Navigator>
-    // </ThemedView>
   );
 }
